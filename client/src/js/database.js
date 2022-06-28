bf56691edff7f84a98a163c979c9a06ce59654af
Erna Lukac <e.lukac4@gmail.com>
2022-06-27 19:31:22 -0600

    database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.error('putDb not implemented');
+  const db = await openDB('jate', 1);
+  const text = db.transaction('jate', 'readwrite');
+  const save = text.objectStore('jate');
+  const access = save.put({ id: 1, value: content });
+  const info = await access;
+  console.log('Virus saved to database', info.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,23 @@
+const { text } = require("express");
+
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
+    textHeader.textContent = "Don't Click.. or do";
+    });
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Why did you click me? installed virus';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = "Virus installed";
+});
